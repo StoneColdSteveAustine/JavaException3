716934cffd19d644dc79e4c182cd77d7e52f259e
StoneColdSteveAustine <115403281+StoneColdSteveAustine@users.noreply.github.com>
2023-08-11 18:53:26 +0600

    Create B_DateException.java

---
+++ B_DateException.java
@@ -0,0 +1,12 @@
+package JavaException3;
+
+public class DateExceptions extends Exceptions {
+    public B_DateException() {
+    }
+
+    public void dataExceptions(String i) {
+        System.out.println("Exception: DateNotCorrectFormat");
+        System.out.printf("Ошибка: неверный формат даты %s", i);
+        System.out.println();
+    }
+}
